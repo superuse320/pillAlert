d0f403ca9143990c5ef28a8cb8604cac2853ac07
eberKachi <eber.nina96@gmail.com>
2021-10-25 10:55:33 -0400

    quitando componentes

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,7 +10,7 @@ export class LoginService {
   url: string;
   isLoggedIn = false;
   token: any;
-  data: any;
+
   constructor(private http: HttpClient,
   ) {
     this.url = environment.url;
@@ -28,17 +28,10 @@ export class LoginService {
       }),
     );
   }
-  register(email: string,name: string,username: string,password: string){ 
-  //register(data: any) {
+
+  register(data: any) {
     return this.http.post(this.url + '/auth/register',
-    {email,name,username,password}
-    ).pipe(
-      tap(data => {
-        localStorage.setItem('token', JSON.stringify(data));
-        this.token = data;
-        this.isLoggedIn = true;
-        return data;
-      }),
+      data
     );
   }
 
